293217acf3935ba1e6332283622b60aa464f8e66
Matias <elmatiastomas@gmail.com>
2025-10-20 17:18:45 -0300

    Se agregan metodos de obtener usuarios y obtener usuario por id

--- Trabajo Grupales/Grupo N° 2/controllers/usuarios.controller.js
+++ Trabajo Grupales/Grupo N° 2/controllers/usuarios.controller.js
@@ -1,6 +1,6 @@
 import db from "../config/db.js";
 
-export const crearUsuario = async (req, res) => {
+export const crearUsuario = async (req, res) => { 
   try {
     const { correoUsuario, contraseñaUsuario } = req.body;
 
@@ -29,5 +29,35 @@ export const modificarUsuario = async (req, res) => {
     } catch (error) {
         console.error("Error en modificar Usuario:", error);
         res.status(500).json({ error: "Error en modificar usuario" });
+    }
+};
+
+//Obtener todos los Usuarios
+
+export const obtenerUsuarios = async (req, res) => {
+    try {
+        const query = "SELECT idUsuario, correoUsuario, contraseñaUsuario FROM usuarios";
+        const [result] = await db.query(query);
+        res.status(200).json(result);
+    }
+    catch (error) { 
+        console.error("Error en obtener Usuarios:", error);
+        res.status(500).json({ error: "Error en obtener usuarios" });
+    }
+};
+
+// Obtener usuario por id
+export const obtenerUsuarioPorId = async (req, res) => {
+    try {
+        const { idUsuario } = req.params;
+        const query = "SELECT idUsuario, correoUsuario, contraseñaUsuario FROM usuarios WHERE idUsuario = ?";
+        const [result] = await db.query(query, [idUsuario]);
+        if (result.length === 0) {
+            return res.status(404).json({ error: "Usuario no encontrado" });
+        }
+        res.status(200).json(result[0]);
+    } catch (error) {
+        console.error("Error en obtener Usuario por ID:", error);
+        res.status(500).json({ error: "Error en obtener usuario por ID" });
     }
 };
--- Trabajo Grupales/Grupo N° 2/routes/usuarios.routes.js
+++ Trabajo Grupales/Grupo N° 2/routes/usuarios.routes.js
@@ -1,5 +1,6 @@
 import { Router } from "express";
-import { CrearUsuario, modificarUsuario} from "../controllers/usuario.controller.js";
+import { CrearUsuario, modificarUsuario,} from "../controllers/usuario.controller.js";
+import { obtenerUsuarioPorId, obtenerUsuarios } from "../controllers/usuarios.controller.js";
 
 
 const router = Router()
@@ -14,6 +15,8 @@ const router = Router()
 
 router.post("/registro", CrearUsuario)
 router.put("/modificar/:idUsuario", modificarUsuario)
+router.get("/usuarios", obtenerUsuarios)
+router.get("/usuarios/:idUsuario", obtenerUsuarioPorId)
 
 export default router;
 
