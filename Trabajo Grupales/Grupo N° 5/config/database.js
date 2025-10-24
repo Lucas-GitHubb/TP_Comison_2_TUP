0b320c14c7d9f5ac3791c0f03e863da1db6b3eea
Luis Chrestia <chrestia.luis@gmail.com>
2025-10-24 09:53:30 -0300

    jwt done

--- Trabajo Grupales/Grupo N° 5/config/database.js
+++ Trabajo Grupales/Grupo N° 5/config/database.js
@@ -2,7 +2,7 @@ const mysql2 = require("mysql2")
 
 const conection = mysql2.createConnection({
     host: process.env.DB_HOST,
-    port: 3307,
+    port: process.env.DB_PORT || 3306,
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
